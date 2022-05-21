a9df4197206cca1ec54cbc73fcd63c2a007edfbf
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-05-21 09:59:36 +0200

    first commit

---
+++ gen.zsh
@@ -0,0 +1,84 @@
+#! /bin/zsh
+
+## Get version number to download and patch
+if [ -z "$1" ]; then
+	read "discordver?Version number to patch: "
+else
+	discordver="$1"
+fi
+
+## Make clean tmp dir
+rm -rf /tmp/aliucord
+mkdir /tmp/aliucord
+
+## Download hermes native libraries
+cd /tmp/aliucord
+mkdir downloads
+cd /tmp/aliucord/downloads
+wget "https://nightly.link/TymanWasTaken/AliuHermes/actions/artifacts/241134965.zip" -O /tmp/aliucord/downloads/android.zip
+unzip android.zip
+cd /tmp/aliucord
+
+## Iterate over all discord architectures to download apks and replace native libs
+mkdir /tmp/aliucord/apks/unsigned -p
+architectures_url=(x86 x86_64 arm64_v8a armeabi_v7a)
+architectures_zip=(x86 x86_64 arm64-v8a armeabi-v7a)
+for i in {1..$#architectures_url}; do
+	# Download config apk
+	wget "https://aliucord.com/download/discord?v=$discordver&split=config.${architectures_url[i]}" -O "/tmp/aliucord/apks/unsigned/config.${architectures_url[i]}.apk"
+
+	# Extract libs from hermes
+	mkdir "/tmp/aliucord/lib/${architectures_zip[i]}" -p
+	unzip -p /tmp/aliucord/downloads/hermes-cppruntime-release.aar "jni/${architectures_zip[i]}/libc++_shared.so" > "/tmp/aliucord/lib/${architectures_zip[i]}/libc++_shared.so"
+	unzip -p /tmp/aliucord/downloads/hermes-release.aar "jni/${architectures_zip[i]}/libhermes.so" > "/tmp/aliucord/lib/${architectures_zip[i]}/libhermes.so"
+
+	# Replace libs in config split
+	zip -0u "/tmp/aliucord/apks/unsigned/config.${architectures_url[i]}.apk" "lib/${architectures_zip[i]}/libhermes.so"
+	zip -0u "/tmp/aliucord/apks/unsigned/config.${architectures_url[i]}.apk" "lib/${architectures_zip[i]}/libc++_shared.so"
+done
+
+## Download AliucordNative
+wget "https://nightly.link/Aliucord/AliucordNative/workflows/android/main/AliucordNative.zip" -O /tmp/aliucord/downloads/AliucordNative.zip
+unzip -p /tmp/aliucord/downloads/AliucordNative.zip classes.dex > /tmp/aliucord/downloads/AliucordNative.dex
+
+## Download and patch base apk
+wget "https://aliucord.com/download/discord?v=$discordver" -O /tmp/aliucord/downloads/base.apk
+cd downloads
+apktool d --no-src base.apk
+wget "https://cdn.discordapp.com/attachments/925873645593722970/977022955492098088/manifest.patch" -O /tmp/aliucord/downloads/manifest.patch
+cd base
+patch AndroidManifest.xml ../manifest.patch
+for f in ./classes?.dex(On); do
+	OLD_NUM="${f//\.(\/classes|dex)/}"
+	NEW_NUM=$((OLD_NUM+1))
+	mv $f "${f/$OLD_NUM/$NEW_NUM}"
+done
+mv classes.dex classes2.dex
+cp /tmp/aliucord/downloads/AliucordNative.dex classes.dex
+cd ..
+apktool b base
+cd base/build/apk
+
+## Replace all necessary files in base.apk
+zip -u /tmp/aliucord/downloads/base.apk AndroidManifest.xml
+for dex in ./classes*.dex; do
+	zip -u /tmp/aliucord/downloads/base.apk $dex
+done
+cp /tmp/aliucord/downloads/base.apk /tmp/aliucord/apks/unsigned/base.apk
+
+## Download rest of the splits
+# Lanuage splits
+wget "https://aliucord.com/download/discord?v=$discordver&split=config.en" -O /tmp/aliucord/apks/unsigned/config.en.apk
+wget "https://aliucord.com/download/discord?v=$discordver&split=config.de" -O /tmp/aliucord/apks/unsigned/config.de.apk
+# DPI Splits
+wget "https://aliucord.com/download/discord?v=$discordver&split=config.hdpi" -O /tmp/aliucord/apks/unsigned/config.hdpi.apk
+wget "https://aliucord.com/download/discord?v=$discordver&split=config.xxhdpi" -O /tmp/aliucord/apks/unsigned/config.xxhdpi.apk
+
+## Sign all apks
+uber-apk-signer --apks /tmp/aliucord/apks/unsigned/ --allowResign --out /tmp/aliucord/apks/
+
+## Disassemble .bundle file
+cd /tmp/aliucord/downloads
+unzip -p base.apk assets/index.android.bundle > index.android.bundle
+hbcdump index.android.bundle -human -pretty-disassemble -out=bytecode.hbc -c="disassemble;quit"
+cp bytecode.hbc ../apks/

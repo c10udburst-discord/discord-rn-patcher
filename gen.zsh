08944045dc196377073960f12aa004fecee1514e
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-05-21 16:24:44 +0200

    im stupid

--- gen.zsh
+++ gen.zsh
@@ -36,12 +36,12 @@ architectures_zip=(x86 x86_64 arm64-v8a armeabi-v7a)
 
 unzip -o /tmp/aliucord/downloads/hermes-cppruntime-release.aar
 unzip -o /tmp/aliucord/downloads/hermes-release.aar
-mkdir lib
 for i in {1..$#architectures_url}; do
 	# Download config apk
 	wget "https://aliucord.com/download/discord?v=$discordver&split=config.${architectures_url[i]}" -O "/tmp/aliucord/apks/unsigned/config.${architectures_url[i]}.apk"
 
 	# configs need libs/ folder
+	mkdir -p "lib/${architectures_zip[i]}"
 	cp "jni/${architectures_zip[i]}/libhermes.so" "lib/${architectures_zip[i]}/libhermes.so"
 	cp "jni/${architectures_zip[i]}/libc++_shared.so" "lib/${architectures_zip[i]}/libc++_shared.so"
 
